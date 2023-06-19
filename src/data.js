d6456f3717e1c566ea07b309f29c1b38a2e2554a
ArunachalamTringapps <arunachalam.muthuvel@tringapps.com>
2023-06-19 10:53:22 +0530

    updated verison

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const product_card = [
         product_name: "Rayes Alpha",
         description: "Hands-free, Hads-on Mushc Experience",
         price: 350,
-        currency: "₹",
+        currency: "$",
         thumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2pwoTOKjnLxbUc3QmYTAPZJRttMADT_HOYQ&usqp=CAU",
         amount:1
     },
